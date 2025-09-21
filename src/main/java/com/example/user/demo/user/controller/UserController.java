90b2f084ca955cf4337f88dcc749bc4961c0a722
unknown <shrinivasirakal7@gmail.com>
2025-09-21 18:46:37 +0530

    Local change to create conflict

--- src/main/java/com/example/user/demo/user/controller/UserController.java
+++ src/main/java/com/example/user/demo/user/controller/UserController.java
@@ -22,7 +22,8 @@ public class UserController {
         User save = userRepository.save(user);
         return new ResponseEntity<>(save, HttpStatus.OK);
     }
-        System.out.println("conflict from local");
+        System.out.println("Local change for conflict");
+
     @DeleteMapping
     public ResponseEntity<String> deleteUser(@PathVariable long id){
         userRepository.deleteById(id);
